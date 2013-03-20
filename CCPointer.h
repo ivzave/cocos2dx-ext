9f5cf861671ef333f3f6951cb2f174fa0908b3ef
ivzave <ivzave@gmail.com>
2013-03-21 00:07:01 +0400

    update CCPointer

--- CCPointer.h
+++ CCPointer.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-Copyright (c) 2012 Igor Zavorotkin
+Copyright (c) 2012-2013 Igor Zavorotkin <ivzave@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,36 +23,11 @@ THE SOFTWARE.
 #ifndef CCPOINTER_H_
 #define CCPOINTER_H_
 
-#include "ccMacros.h"
+#include "CCCommonExtended.h"
 
 NS_CC_BEGIN
 
-template<typename T>
-class CCReleaser
-{
-public:
-	CCReleaser(T* pObject) :
-		m_pObject(pObject)
-	{
-	}
-
-	~CCReleaser()
-	{
-		CC_SAFE_RELEASE(m_pObject);
-	}
-
-private:
-	T* m_pObject;
-
-	template <typename U>
-	friend class CCPointer;
-};
-
-template<typename T>
-CCReleaser<T> CCReleaserMake(T* pObject)
-{
-	return CCReleaser<T>(pObject);
-}
+/** Smart pointer class for use with Cocos2d-x */
 
 template<typename T>
 class CCPointer
@@ -61,6 +36,12 @@ public:
 	CCPointer() :
 		m_pObject(NULL)
 	{
+	}
+
+	CCPointer(T* pObject) :
+		m_pObject(pObject)
+	{
+		CC_SAFE_RETAIN(m_pObject);
 	}
 
 	template<typename U>
@@ -79,19 +60,6 @@ public:
 	template<typename U>
 	CCPointer(const CCPointer<U>& tPointer) :
 		m_pObject(tPointer.m_pObject)
-	{
-		CC_SAFE_RETAIN(m_pObject);
-	}
-
-	CCPointer(const CCReleaser<T>& tHolder) :
-		m_pObject(tHolder.m_pObject)
-	{
-		CC_SAFE_RETAIN(m_pObject);
-	}
-
-	template<typename U>
-	CCPointer(const CCReleaser<U>& tHolder) :
-		m_pObject(tHolder.m_pObject)
 	{
 		CC_SAFE_RETAIN(m_pObject);
 	}
@@ -136,44 +104,37 @@ public:
 		return *this;
 	}
 
-	CCPointer& operator=(const CCReleaser<T>& tHolder)
-	{
-		if (m_pObject != tHolder.m_pObject)
-		{
-			CC_SAFE_RELEASE(m_pObject);
-			m_pObject = tHolder.m_pObject;
-			CC_SAFE_RETAIN(m_pObject);
-		}
-		return *this;
-	}
-
-	template<typename U>
-	CCPointer& operator=(const CCReleaser<U>& tHolder)
-	{
-		if (m_pObject != tHolder.m_pObject)
-		{
-			CC_SAFE_RELEASE(m_pObject);
-			m_pObject = tHolder.m_pObject;
-			CC_SAFE_RETAIN(m_pObject);
-		}
-		return *this;
-	}
-
 	T* operator->()
 	{
-		CCAssert(m_pObject != NULL, "CCPointer: content not set");
+		CCDebugEnsureCustom(m_pObject != NULL, "dereferencing null pointer");
 		return m_pObject;
 	}
 
 	const T* operator->() const
 	{
-		CCAssert(m_pObject != NULL, "CCPointer: content not set");
+		CCDebugEnsureCustom(m_pObject != NULL, "dereferencing null pointer");
 		return m_pObject;
+	}
+
+	bool operator==(T* pObject) const
+	{
+		return m_pObject == pObject;
+	}
+
+	bool operator!= (T* pObject) const
+	{
+		return m_pObject != pObject;
 	}
 
 	operator T*() const
 	{
 		return m_pObject;
+	}
+
+	template<typename U>
+	operator U*() const
+	{
+		return static_cast<U*>(m_pObject);
 	}
 
 private:
@@ -183,28 +144,46 @@ private:
 	friend class CCPointer;
 };
 
+/** Helper function for CCPointer. Use it to create smart pointers to objects which need to be released (e.g. created with operator "new") */
+
+template<typename T>
+CCPointer<T> CCPointerGrab(T* pObject)
+{
+	CCPointer<T> tRet(pObject);
+	pObject->release();
+	return tRet;
+}
+
 NS_CC_END
 
-#define CC_PROPERTY_READONLY_RETAIN_EXT(varType, varName, funName) \
+/** Declare protected smart pointer member variable with public getter */
+
+#define CC_PROPERTY_READONLY_SMART(varType, varName, funName) \
 protected: \
 	cocos2d::CCPointer<varType> varName; \
 public: \
 	virtual varType* get##funName() const;
 
-#define CC_PROPERTY_RETAIN_EXT(varType, varName, funName) \
+/** Declare protected smart pointer member variable with public getter and setter */
+
+#define CC_PROPERTY_SMART(varType, varName, funName) \
 protected: \
 	cocos2d::CCPointer<varType> varName; \
 public: \
 	virtual varType* get##funName(); \
 	virtual void set##funName(varType* var);
 
-#define CC_SYNTHESIZE_READONLY_RETAIN_EXT(varType, varName, funName) \
+/** Declare protected smart pointer member variable with generated public getter */
+
+#define CC_SYNTHESIZE_READONLY_SMART(varType, varName, funName) \
 protected: \
 	cocos2d::CCPointer<varType> varName; \
 public: \
 	virtual varType* get##funName() const { return varName; }
 
-#define CC_SYNTHESIZE_RETAIN_EXT(varType, varName, funName) \
+/** Declare protected smart pointer member variable with generated public getter and setter */
+
+#define CC_SYNTHESIZE_SMART(varType, varName, funName) \
 protected: \
 	cocos2d::CCPointer<varType> varName; \
 public: \
@@ -212,4 +191,3 @@ public: \
 	virtual void set##funName(varType* var) { varName = var; }
 
 #endif /* CCPOINTER_H_ */
-
